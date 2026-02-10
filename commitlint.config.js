61161126405f53cb79e553e53f6c2a80cd7709d7
GX88 <gxlove_max@163.com>
2026-02-10 14:43:36 +0800

    chore: 优化提交配置

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+// commit-lint config
+export default {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // @see https://commitlint.js.org/#/reference-rules
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'types',
+      ],
+    ],
+  },
+}
