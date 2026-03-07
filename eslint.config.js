805cf875d11b085732bd57be95ee3373beb0f9bd
GX88 <gxlove_max@163.com>
2026-03-07 17:13:48 +0800

    chore: 更新 ESLint 配置，优化忽略规则和修复 lint 脚本

--- eslint.config.js
+++ eslint.config.js
@@ -3,13 +3,24 @@ import vueI18n from '@intlify/eslint-plugin-vue-i18n'
 
 export default antfu(
   {
-    ignores: ['public', 'dist*', '**/*.d.ts', 'src/renderer/src/types/*.d.ts'],
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/dist-*/**',
+      '**/out/**',
+      '**/release/**',
+      '**/.vite/**',
+      '**/coverage/**',
+      '**/*.d.ts',
+      'src/renderer/src/types/*.d.ts',
+      'public/**',
+    ],
   },
   {
     rules: {
       'eslint-comments/no-unlimited-disable': 'off', // 禁止使用无限禁用注释
       'no-irregular-whitespace': 'off', // 禁止不规则的空格
-      'style/max-statements-per-line': ['error', { max: 3 }], // 限制每行最大语句数为 3
+      'style/max-statements-per-line': ['error', { max: 1 }], // 限制每行最大语句数为 1
       'curly': ['error', 'all'],
       'ts/no-unused-expressions': [
         'error',
